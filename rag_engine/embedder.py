0f0a5f4dd370fe9cc5cf33d628b723cdc537ffa2
Aman <akgarg1107@gmail.com>
2025-05-09 19:06:03 +0530

    Fixed hugging face

--- rag_engine/embedder.py
+++ rag_engine/embedder.py
@@ -6,6 +6,7 @@ _model = None
 def get_embedding(text: str):
     global _model
     if _model is None:
-        _model = INSTRUCTOR("hkunlp/instructor-base")  # Load only once when first used
+        print("Loading INSTRUCTOR model...")
+        _model = INSTRUCTOR("hkunlp/instructor-base")
     instruction = "Represent the news article for retrieval:"
     return _model.encode([[instruction, text]])[0].tolist()
