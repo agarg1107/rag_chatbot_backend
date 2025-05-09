b12d8c0a627e3098f3cb8afbcbab3f20006fdd16
Aman <akgarg1107@gmail.com>
2025-05-10 00:05:54 +0530

    chnage the model

--- rag_engine/embedder.py
+++ rag_engine/embedder.py
@@ -13,21 +13,21 @@
 
 
 
-# from sentence_transformers import SentenceTransformer
-
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# def get_embedding(text: str):
-#     return model.encode(text).tolist()
-
-
 from sentence_transformers import SentenceTransformer
 
-_model = None
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_embedding(text: str):
-    global _model
-    if _model is None:
-        _model = SentenceTransformer("all-MiniLM-L6-v2")
-    return _model.encode(text).tolist()
+    return model.encode(text).tolist()
+
+
+# from sentence_transformers import SentenceTransformer
+
+# _model = None
+
+# def get_embedding(text: str):
+#     global _model
+#     if _model is None:
+#         _model = SentenceTransformer("all-MiniLM-L6-v2")
+#     return _model.encode(text).tolist()
 
