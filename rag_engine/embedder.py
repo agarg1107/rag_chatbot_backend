c5352cb2bc3d7f7b60dd4c8badb2041472577768
Aman <akgarg1107@gmail.com>
2025-05-09 21:32:13 +0530

    Fixed memory issue

--- rag_engine/embedder.py
+++ rag_engine/embedder.py
@@ -11,9 +11,23 @@
 #     instruction = "Represent the news article for retrieval:"
 #     return _model.encode([[instruction, text]])[0].tolist()
 
+
+
+# from sentence_transformers import SentenceTransformer
+
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# def get_embedding(text: str):
+#     return model.encode(text).tolist()
+
+
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+_model = None
 
 def get_embedding(text: str):
-    return model.encode(text).tolist()
+    global _model
+    if _model is None:
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+    return _model.encode(text).tolist()
+
